e1dfb5afa110724783374818359f5dc8528c8de4
Shahdd21 <88803131+Shahdd21@users.noreply.github.com>
2025-02-24 22:29:06 +0200

    added wallet creation

--- src/main/java/com/project/e_commerce_api/auth/AuthenticationService.java
+++ src/main/java/com/project/e_commerce_api/auth/AuthenticationService.java
@@ -1,21 +1,17 @@
 package com.project.e_commerce_api.auth;
 
 import com.project.e_commerce_api.config.JwtService;
-import com.project.e_commerce_api.entity.Customer;
-import com.project.e_commerce_api.entity.User;
-import com.project.e_commerce_api.entity.Vendor;
-import com.project.e_commerce_api.entity.Wishlist;
+import com.project.e_commerce_api.entity.*;
 import com.project.e_commerce_api.enums.UserRole;
-import com.project.e_commerce_api.repository.CustomerRepository;
-import com.project.e_commerce_api.repository.UserRepository;
-import com.project.e_commerce_api.repository.VendorRepository;
-import com.project.e_commerce_api.repository.WishlistRepository;
+import com.project.e_commerce_api.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 @Service
@@ -25,16 +21,18 @@ public class AuthenticationService {
     private final CustomerRepository customerRepository;
     private final VendorRepository vendorRepository;
     private final WishlistRepository wishlistRepository;
+    private final WalletRepository walletRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthenticationService(UserRepository userRepository, CustomerRepository customerRepository, VendorRepository vendorRepository, WishlistRepository wishlistRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+    public AuthenticationService(UserRepository userRepository, CustomerRepository customerRepository, VendorRepository vendorRepository, WishlistRepository wishlistRepository, WalletRepository walletRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.customerRepository = customerRepository;
         this.vendorRepository = vendorRepository;
         this.wishlistRepository = wishlistRepository;
+        this.walletRepository = walletRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
@@ -48,15 +46,21 @@ public class AuthenticationService {
         User user = new User(request.getFirstName()+"user", passwordEncoder.encode(request.getPassword()),
                 UserRole.ROLE_CUSTOMER, true);
 
-        userRepository.save(user);
-        customer.setUser(user);
+        User savedUser = userRepository.save(user);
+        customer.setUser(savedUser);
         Customer savedCustomer = customerRepository.save(customer);
 
         Wishlist wishlist = new Wishlist();
         wishlist.setCustomer(savedCustomer);
         wishlistRepository.save(wishlist);
 
-        var jwtToken = jwtService.generateToken(user);
+        Wallet wallet = new Wallet();
+        wallet.setCustomer(savedCustomer);
+        wallet.setBalance(BigDecimal.ZERO);
+        wallet.setUpdatedAt(LocalDate.now());
+        walletRepository.save(wallet);
+
+        var jwtToken = jwtService.generateToken(savedUser);
 
         return new AuthenticationResponse(jwtToken);
     }
