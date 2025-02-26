e4a2c71fa1c1ef5105858b5d92dd041472fdc31c
Shahdd21 <88803131+Shahdd21@users.noreply.github.com>
2025-02-26 05:43:19 +0200

    added acess for super admin

--- src/main/java/com/project/e_commerce_api/config/SecurityConfig.java
+++ src/main/java/com/project/e_commerce_api/config/SecurityConfig.java
@@ -1,27 +1,32 @@
 package com.project.e_commerce_api.config;
 
+import com.project.e_commerce_api.auth.AuthenticationEntryPointImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableMethodSecurity
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationEntryPointImp authenticationEntryPoint;
 
     @Autowired
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider, AuthenticationEntryPointImp authenticationEntryPoint) {
         this.jwtAuthFilter = jwtAuthFilter;
         this.authenticationProvider = authenticationProvider;
+        this.authenticationEntryPoint = authenticationEntryPoint;
     }
 
     @Bean
@@ -31,30 +36,30 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").hasAnyRole("SUPER_ADMIN","ADMIN")
                         .requestMatchers(HttpMethod.GET,"/products/**").permitAll()
                         .requestMatchers(HttpMethod.POST,"/products").hasRole("VENDOR")
                         .requestMatchers(HttpMethod.PUT,"/products/**").hasRole("VENDOR")
                         .requestMatchers(HttpMethod.PATCH,"/products/**").hasRole("VENDOR")
-                        .requestMatchers(HttpMethod.DELETE,"/products/**").hasAnyRole("VENDOR", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/products/**").hasAnyRole("VENDOR", "ADMIN", "SUPER_ADMIN")
                         .requestMatchers(HttpMethod.GET, "/orders").hasRole("CUSTOMER")
-                        .requestMatchers(HttpMethod.GET, "/orders/{orderId:\\\\d+}").hasAnyRole("CUSTOMER","ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/orders/{orderId:\\\\d+}").hasAnyRole("CUSTOMER","ADMIN", "SUPER_ADMIN")
                         .requestMatchers(HttpMethod.PATCH, "/orders/{orderId:\\\\d+}/cancel").hasRole("CUSTOMER")
-                        .requestMatchers(HttpMethod.GET, "/orders/all").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PATCH, "/orders/{orderId:\\\\d+}/status").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/orders/all").hasAnyRole("ADMIN", "SUPER_ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/orders/{orderId:\\\\d+}/status").hasAnyRole("ADMIN","SUPER_ADMIN")
                         .requestMatchers(HttpMethod.POST, "/payments/**").hasRole("CUSTOMER")
-                        .requestMatchers(HttpMethod.GET, "/payments/**").hasAnyRole("CUSTOMER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/payments/**").hasAnyRole("CUSTOMER", "ADMIN", "SUPER_ADMIN")
                         .requestMatchers(HttpMethod.GET, "/reviews/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/reviews/{productId:\\\\d+}").hasRole("CUSTOMER")
                         .requestMatchers(HttpMethod.PATCH, "/reviews/{reviewId:\\\\d+}").hasRole("CUSTOMER")
-                        .requestMatchers(HttpMethod.DELETE, "/reviews/{reviewId:\\\\d+}").hasAnyRole("CUSTOMER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/reviews/{reviewId:\\\\d+}").hasAnyRole("CUSTOMER", "ADMIN", "SUPER_ADMIN")
                         .requestMatchers(HttpMethod.GET, "/wishlist").hasRole("CUSTOMER")
                         .requestMatchers(HttpMethod.POST, "/wishlist/{productId:\\\\d+}").hasRole("CUSTOMER")
                         .requestMatchers(HttpMethod.DELETE, "/wishlist/{productId:\\\\d+}").hasRole("CUSTOMER")
                         .requestMatchers(HttpMethod.GET, "/categories").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/categories").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PATCH, "/categories/{categoryId:\\\\d+}").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/categories/{categoryId:\\\\d+}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/categories").hasAnyRole("ADMIN", "SUPER_ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/categories/{categoryId:\\\\d+}").hasAnyRole("ADMIN", "SUPER_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/categories/{categoryId:\\\\d+}").hasAnyRole("ADMIN", "SUPER_ADMIN")
                         .requestMatchers(HttpMethod.GET, "/vendors").permitAll()
                         .requestMatchers("/wallet").hasRole("CUSTOMER")
 
@@ -62,6 +67,9 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(authenticationEntryPoint)
                 )
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
